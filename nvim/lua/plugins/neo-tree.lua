afa4893bac5bbb9574f62de455b0a07ecc9e8362
Luis Sotomayor <luis@zuub.com>
2024-04-28 13:30:48 -0700

    feat: add preview capabilities

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -3,6 +3,20 @@ return {
     branch = "v3.x",
     dependencies = {"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim"},
     config = function()
+        require("neo-tree").setup({
+            window = {
+                mappings = {
+                    ["P"] = {
+                        "toggle_preview",
+                        config = {
+                            use_float = true,
+                            use_image_nvim = true
+                        }
+                    }
+                }
+            }
+        })
+
         vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<CR>", {})
     end
 }
