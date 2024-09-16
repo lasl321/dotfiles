f0c86f1c1539822afcc09475cfcdaa2f2dd9b1b2
Luis Sotomayor <luis@zuub.com>
2024-09-15 21:09:38 -0700

    feat: add wezterm configuration

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,31 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+local config = wezterm.config_builder()
+
+-- Font items
+config.font = wezterm.font("MesloLGS NF")
+config.font_size = 13
+
+-- Do not show window tab bar
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+
+-- Set up transparency
+config.window_background_opacity = 0.85
+config.macos_window_background_blur = 0
+
+-- Use iTerm2 color scheme
+config.color_scheme = "iTerm2 Default"
+
+config.keys = {{
+    key = 'k',
+    mods = 'SUPER',
+    action = act.Multiple {act.ClearScrollback 'ScrollbackAndViewport', act.SendKey {
+        key = 'L',
+        mods = 'CTRL'
+    }}
+}}
+
+return config
