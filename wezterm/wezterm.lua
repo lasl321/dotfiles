5de8cc8678b7670829f54adbe37ac7a4a998c776
Luis Sotomayor <luis@zuub.com>
2025-01-25 16:55:27 -0800

    feat: add custom TERM value

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,6 +3,9 @@ local act = wezterm.action
 
 local config = wezterm.config_builder()
 
+-- Wezterm custom TERM value
+config.term = "wezterm"
+
 -- Font items
 config.font = wezterm.font("MesloLGS NF")
 config.font_size = 13
@@ -19,13 +22,18 @@ config.macos_window_background_blur = 0
 -- Use iTerm2 color scheme
 config.color_scheme = "iTerm2 Default"
 
-config.keys = {{
-    key = 'k',
-    mods = 'SUPER',
-    action = act.Multiple {act.ClearScrollback 'ScrollbackAndViewport', act.SendKey {
-        key = 'L',
-        mods = 'CTRL'
-    }}
-}}
+config.keys = {
+	{
+		key = "k",
+		mods = "SUPER",
+		action = act.Multiple({
+			act.ClearScrollback("ScrollbackAndViewport"),
+			act.SendKey({
+				key = "L",
+				mods = "CTRL",
+			}),
+		}),
+	},
+}
 
 return config
